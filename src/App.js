876920df3445555931cff0051c3985f9127b8126
JuanpabloSG <juan.sanchezgz@udlap.mx>
2021-02-08 19:56:06 -0500

    tarea de foco

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      textToShow: "Hola Koders!!!",
+      isOn: false
+    }
+    this.changeHandler = this.changeHandler.bind(this)
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+
+  changeHandler( event ){
+    let newText = event.target.value
+    this.setState({ textToShow : newText})
+  }
+
+  clickHandler( event ){
+    this.setState({ isOn : !this.state.isOn })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <h1> {this.state.textToShow} </h1>
+        <input type="text" onChange={ this.changeHandler }/>
+        <div className={ this.state.isOn ? "foco foco-on" : "foco"}></div>
+        <button type="button" onClick={ this.clickHandler }> Encender </button>
+      </div>
+    )
+  }
 }
 
 export default App;
